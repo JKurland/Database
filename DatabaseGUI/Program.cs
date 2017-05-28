0ab57f5d202ff93867717d3d90122784f6eed8dc
JKurland <joshua.kurland@outlook.com>
2017-05-28 11:10:14 +0100

    First Commit

---
+++ DatabaseGUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Gtk;
+
+namespace DatabaseGUI
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.Show();
+            Application.Run();
+        }
+    }
+}
