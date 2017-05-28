bdfeb05bc4d29007168551884bae1095c495b01f
JKurland <joshua.kurland@outlook.com>
2017-05-28 13:40:34 +0100

    Started switchboard

--- DatabaseGUI/MainWindow.cs
+++ DatabaseGUI/MainWindow.cs
@@ -13,4 +13,8 @@ public partial class MainWindow : Gtk.Window
         Application.Quit();
         a.RetVal = true;
     }
+
+    protected void Window1Open(object sender, EventArgs e)
+    {
+    }
 }
